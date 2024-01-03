61baafd648ec8bca2574663483bf4f3094b88a49
FlintMischiff <Flintmischiff@gmail.com>
2024-01-03 17:02:59 -0500

    Initial commit

---
+++ src/main/java/net/flintmischiff/theovergrowth/TheOvergrowth.java
@@ -0,0 +1,16 @@
+package net.flintmischiff.theovergrowth;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheOvergrowth implements ModInitializer {
+	public static final String MOD_ID = "theovergrowth";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/flintmischiff/theovergrowth/TheOvergrowthClient.java
@@ -0,0 +1,10 @@
+package net.flintmischiff.theovergrowth;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheOvergrowthClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
